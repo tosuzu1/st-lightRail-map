5fc9557420434366d3fba7340ce0e658d8b7d762
felix.hong <felix.hong0321@gmail.com>
2024-12-19 06:27:35 -0800

    moved javascript to external file

---
+++ getTime.js
@@ -0,0 +1,7 @@
+function updateTime() {
+    let now = new Date();
+    let formattedTime = now.toLocaleTimeString(); 
+    document.getElementById("timedisplay").textContent = Date();
+}
+
+setInterval(updateTime, 1000); // Update every second
