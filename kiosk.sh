2c0df2bad2fb54b5390bf9b5cc2b163acd277326
Felix Hong <felix.hong0321@gmail.com>
2025-02-17 14:52:13 -0800

    run on pi

--- kiosk.sh
+++ kiosk.sh
@@ -4,9 +4,9 @@ KIOSK_URL="127.0.0.1:1234/"
 cd /home/felix/Desktop/st-lightRail-map
 
 npm start &
-
+#/home/felix/.nvm/versions/node/v23.8.0/bin/npm start &
 # Wait for services to come online.
 sleep 10
 
 echo 'Starting Chromium...'
-/usr/bin/chromium-browser $KIOSK_URL --noerrdialogs --disable-infobars --kiosk --no-first-run --ozone-platform=wayland --enable-features=OverlayScrollbar --start-maximized
+/usr/bin/chromium-browser 127.0.0.1:1234/ --noerrdialogs --disable-infobars --kiosk  --start-maximized
